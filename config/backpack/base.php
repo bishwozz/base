fc45aac1516c6315c05ff9d8816bd0f69146e55b
bishwozz <bishwo123sapkota@gmail.com>
2023-09-15 10:25:13 +0545

    fixes

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -24,7 +24,7 @@ return [
     // ----
 
     // Project name. Shown in the window title.
-    'project_name' => 'LIBT',
+    'project_name' => 'WEB',
 
     // When clicking on the admin panel's top-left logo/name,
     // where should the user be redirected?
@@ -73,7 +73,7 @@ return [
     // ------
 
     // Menu logo. You can replace this with an <img> tag if you have a logo.
-    'project_logo'   => '<b>Lincon </b>College',
+    'project_logo'   => '<b>Dash </b>Board',
 
     // Show / hide breadcrumbs on admin panel pages.
     'breadcrumbs' => false,
